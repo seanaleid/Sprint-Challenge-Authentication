4d0a7e45dc6192fa011a5706017be5d45969cb25
seanaleid <seanaleid@gmail.com>
2019-11-15 11:45:51 -0500

    Adds code to authenticate-middleware.js and functions when accessing GET '/api/jokes/' endpoint

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,26 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || 'this is the safest secret!';
+   
+    // check that the token is valid
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if(err){
+        // token has been tampered with 
+        res.status(401).json({ message: 'Invalid Credentials' });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  }
 };
