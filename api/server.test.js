26e15299c3f73600e9a12d91bfc160ef0e6efaff
seanaleid <seanaleid@gmail.com>
2019-11-15 13:50:32 -0500

    Adds two tests for the /jokes/ endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -10,7 +10,7 @@ describe('endpoint tests', function() {
             await db('users').truncate();
         })
         
-        it('1) POST /register - should return status 201', function () {
+        it('1) POST /auth/register - should return status 201', function () {
             return request(server)
             .post('/api/auth/register')
             .send({ username: "newUser20", password: "newTest20"})
@@ -19,7 +19,7 @@ describe('endpoint tests', function() {
             })
         })
 
-        it('2) POST /register - res.type should match json', function () {
+        it('2) POST /auth/register - res.type should match json', function () {
             return request(server)
             .post('/api/auth/register')
             .send({ username: "newUser20", password: "newTest20"})
@@ -28,7 +28,7 @@ describe('endpoint tests', function() {
             })
         })
 
-        it('1) POST /login - should return status 200', function () {
+        it('1) POST /auth/login - should return status 200', function () {
             return request(server)
             .post('/api/auth/login')
             .send({ username: "newUser20", password: "newTest20"})
@@ -37,12 +37,28 @@ describe('endpoint tests', function() {
             })
         })
 
-        it('2) POST /login - should return message: "Welcome <username>!"', function () {
+        it('2) POST /auth/login - should return message: "Welcome <username>!"', function () {
             return request(server)
             .post('/api/auth/login')
             .send({ username: "newUser20", password: "newTest20"})
             .then(res => {
                 expect(res.body.message).toMatch(/Welcome newUser20!/i);
+            })
+        })
+
+        it('1) GET /jokes/ - res.type should match json', function () {
+            return request(server)
+            .get('/api/jokes/')
+            .then(res => {
+                expect(res.type).toMatch(/json/i);
+            })
+        })
+
+        it('2) GET /jokes/ - should be defined', function () {
+            return request(server)
+            .get('/api/jokes/')
+            .then(res => {
+                expect(res.body).toBeDefined();
             })
         })
     })
