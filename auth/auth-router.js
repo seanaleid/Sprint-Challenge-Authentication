bb48c2c80f6c48d84c72e7ac18c23f0eb1167a07
seanaleid <seanaleid@gmail.com>
2019-11-15 11:14:21 -0500

    Adds users directory and usersModel.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+
 router.post('/register', (req, res) => {
   // implement registration
 });
